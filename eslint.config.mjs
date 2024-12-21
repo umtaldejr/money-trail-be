f4bfcd0e99e2aea9ea7e1f41e44ea7164656138c
Exupério Lédo Silva Júnior <uspjr.95@hotmail.com>
2024-12-21 17:15:39 -0300

    Set up ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs"
+    }
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      },
+    }
+  },
+  pluginJs.configs.recommended,
+];
