316f7c75aa163d4cfa8f1683a52141c7490ed486
Exupério Lédo Silva Júnior <uspjr.95@hotmail.com>
2024-12-21 18:46:14 -0300

    Implement authentication middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET =  process.env.JWT_SECRET || 'your_secret_key';
+
+exports.authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(403).json({ error: 'Access denied' });
+  }
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Invalid or expired token' });
+    }
+    req.user = user;
+    next();
+  });
+};
